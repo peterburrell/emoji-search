87a7f7238ec9dddc3d081dad59c593cb0f037759
Peter Burrell <peterburrell@gmail.com>
2017-03-20 09:12:08 -0400

    added emojione

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="(value, key) in emoji">
+        {{ String.fromCodePoint(parseInt(value.unicode, 16)) }} {{ key }}
+        {{ value }}
+        <span v-html="toImage(value.shortname)"></span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import emojione from 'emojione';
+import emoji from 'emojione/emoji.json';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'emoji search (comming soon...) 🦄'
+      msg: 'emoji search (comming soon...) 🦄',
+      emoji: emoji
     }
+  },
+  methods: {
+    toImage(input) {
+      return emojione.toImage(input);
+    }
+  },
+  created() {
+    //console.log(emojione);
+    console.log(emoji);
   }
 }
 </script>
@@ -20,12 +40,17 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  width: 800px;
 }
 
 h1, h2 {
   font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
